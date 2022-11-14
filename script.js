bd1d518c86d47cdeab98f855b42bf4ddc88a58b9
gamecattt <ak19gamecat@gmail.com>
2022-11-14 20:36:17 +0300

    stav

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 $('.products-slider').slick({
-  centerMode: true,
-  slidesToShow: 3,
-  variableWidth: true,
-  prevArrow: '.slider .slider__arrow-prev',
-  nextArrow: '.slider .slider__arrow-next',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
-});
+    centerMode: true,
+    slidesToShow: 3,
+    variableWidth: true,
+    prevArrow: '.slider .slider__arrow-prev',
+    nextArrow: '.slider .slider__arrow-next',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
+})
+
+//
+const burger = document.querySelector('.burger')
+const header = document.querySelector('.header')
+
+burger.addEventListener('click', function () {
+    header.classList.toggle('open')
+})
